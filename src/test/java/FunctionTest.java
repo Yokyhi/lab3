50850de3fd6469a61f674c3a1001b762e165379e
Vereshchagin Max <qirl.cry@mail.ru>
2022-12-23 00:45:18 +0300

    Function1

---
+++ src/test/java/FunctionTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+public class FunctionTest {
+    Function function;
+    @BeforeAll
+   public void createObject() {
+    function = new Function(1);
+    }
+    @Test
+    public void functionResult(){
+        Assertions.assertEquals(10.0, Function.func(),0.5);
+    }
+}
